46fed8b8f41872ce5688e184f0317dbfb32701d8
Hamza Dugmag <hdugmag@gmail.com>
2021-02-16 19:18:38 -0500

    folder structure and file naming

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,8 +1,7 @@
-console.log("cash money");
-var p = document.getElementById("productTitle");  // get product title
+// elements of interest
+// var title_span = document.getElementById("productTitle");
 
-// Inform the background page that 
-// this tab should have a page-action.
+// Inform the background page that this tab should have a page-action.
 chrome.runtime.sendMessage({
     from: 'content',
     subject: 'showPageAction',
@@ -10,17 +9,17 @@ chrome.runtime.sendMessage({
 
 // Listen for messages from the popup.
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
+
     // First, validate the message's structure.
     if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+
         // Collect the necessary data. 
-        // (For your specific requirements `document.querySelectorAll(...)`
-        //  should be equivalent to jquery's `$(...)`.)
         var domInfo = {
-            msg: p.innerText,
+            productTitle: document.getElementById("productTitle").innerText,
+            // here
         };
   
-        // Directly respond to the sender (popup), 
-        // through the specified callback.
+        // Directly respond to the sender (popup) through the specified callback.
         response(domInfo);
     }
 });
--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,11 +1,11 @@
 // random parameters
-var images = ["trash.jpg", "bad.jpg", "meh.jpg", "good.jpg", "great.jpg"];
-var hrefs = [
+var ratings = ["img/trash.jpg", "img/bad.jpg", "img/meh.jpg", "img/good.jpg", "img/great.jpg"];
+var altLinks = [
     'https://www.amazon.ca/LK-Tempered-Protector-Shock-Absorption-Protection/dp/B07X2WV87Z/ref=sr_1_5?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729768&sprefix=iphone+case+%2Caps%2C165&sr=8-5',
     'https://www.amazon.ca/Spigen-Ultra-Hybrid-Works-iPhone/dp/B07T2NBLX9/ref=sr_1_6?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729768&sprefix=iphone+case+%2Caps%2C165&sr=8-6',
     'https://www.amazon.ca/OtterBox-Commuter-Case-iPhone-11/dp/B07W45LYY8/ref=sr_1_3?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729837&sprefix=iphone+case+%2Caps%2C165&sr=8-3'
 ];
-var prods = ['LK iPhone 11 Case', "Spigen Ultra Hybrid Case", "OtterBox Commuter Case"]
+var alternatives = ['LK iPhone 11 Case', "Spigen Ultra Hybrid Case", "OtterBox Commuter Case"]
 
 
 // random index generator
@@ -17,19 +17,19 @@ function randint(min, max) {
 
 // Update the relevant fields with the new data.
 const setDOMInfo = info => {
-    document.getElementById('name').innerText = info.msg.substring(0, 35) + "...";  // product title
+    document.getElementById('name').innerText = info.productTitle.substring(0, 35) + "...";  // product title
     
-    if (info.msg.substring(0, 5) == "Otter" || info.msg.substring(0, 5) == "Spige" || info.msg.substring(0, 5) == "LK iP") {
-      document.getElementById("rating").src = images[randint(3, 4)];  // high rating if one of the good ones
+    if (info.productTitle.substring(0, 5) == "Otter" || info.productTitle.substring(0, 5) == "Spige" || info.productTitle.substring(0, 5) == "LK iP") {
+      document.getElementById("rating").src = ratings[randint(3, 4)];  // high rating if one of the good ones
     }
     else {
-        document.getElementById("rating").src = images[randint(0, 2)];  // bad rating if one of the bad ones
+        document.getElementById("rating").src = ratings[randint(0, 2)];  // bad rating if one of the bad ones
         randLink = randint(0, 2);  // random alternative
-        document.getElementById("alt").innerHTML = "<a href=" + hrefs[randLink] + " target='_blank'>" + prods[randLink] + "</a>"
+        document.getElementById("alt").innerHTML = "<a href=" + altLinks[randLink] + " target='_blank'>" + alternatives[randLink] + "</a>"
     }
 
 };
-  
+
 // Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', () => {
     // ...query for the active tab...
