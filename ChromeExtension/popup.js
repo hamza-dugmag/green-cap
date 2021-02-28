23724cc4da8711c40da08550cbd004bdc3d58078
Hamza Dugmag <hdugmag@gmail.com>
2021-02-27 20:35:46 -0500

    cleaning files

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,7 +1,3 @@
-// import * as srt from "./scripts/sorter.js";
-
-
-// ============================================================= from sorter.js
 function get_price(string) {
     string = string.replace(/(<([^›>]+)>)/gi, "");
 
@@ -44,22 +40,7 @@ function get_items(string) {
 }
 // =============================================================
 
-// random parameters
 var ratings = ["img/trash.png", "img/bad.png", "img/meh.png", "img/good.png", "img/great.png"];
-var altLinks = [
-    'https://www.amazon.ca/LK-Tempered-Protector-Shock-Absorption-Protection/dp/B07X2WV87Z/ref=sr_1_5?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729768&sprefix=iphone+case+%2Caps%2C165&sr=8-5',
-    'https://www.amazon.ca/Spigen-Ultra-Hybrid-Works-iPhone/dp/B07T2NBLX9/ref=sr_1_6?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729768&sprefix=iphone+case+%2Caps%2C165&sr=8-6',
-    'https://www.amazon.ca/OtterBox-Commuter-Case-iPhone-11/dp/B07W45LYY8/ref=sr_1_3?crid=2S9OOHKE5XUL9&dchild=1&keywords=iphone+case+11&qid=1611729837&sprefix=iphone+case+%2Caps%2C165&sr=8-3'
-];
-var alternatives = ['LK iPhone 11 Case', "Spigen Ultra Hybrid Case", "OtterBox Commuter Case"]
-
-
-// random index generator
-function randint(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 
 // https://stackoverflow.com/questions/247483/http-get-request-in-javascript
@@ -75,19 +56,10 @@ function httpGet(theUrl)
 // Update the relevant fields with the new data.
 const setDOMInfo = info => {
     document.getElementById("name").innerText = info.productTitle.substring(0, 35) + "...";  // product title
-    
-    // if (info.productTitle.substring(0, 5) == "Otter" || info.productTitle.substring(0, 5) == "Spige" || info.productTitle.substring(0, 5) == "LK iP") {
-    //   document.getElementById("rating").src = ratings[randint(3, 4)];  // high rating if one of the good ones
-    // }
-    // else {
-    //     document.getElementById("rating").src = ratings[randint(0, 2)];  // bad rating if one of the bad ones
-    //     randLink = randint(0, 2);  // random alternative
-    //     document.getElementById("alt").innerHTML = "<a href=" + altLinks[randLink] + " target='_blank'>" + alternatives[randLink] + "</a>"
-    // }
 
     // http request to GreenCap server
-    var price = get_price(info.productPrice);  // IMPORT SORTER
-    var items = get_items(info.productCategories);  // IMPORT SORTER
+    var price = get_price(info.productPrice);
+    var items = get_items(info.productCategories);
     var http_link = "https://l90oikv0ue.execute-api.us-east-2.amazonaws.com/test/greencap?category=" + items[items.length - 1] + "&price=" + price.toString();
     json_get = JSON.parse(httpGet(http_link));
     
